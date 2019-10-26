fe5a2241651aeed37966a0054147a79a96cc85dc
unknown <Debbie@nb-debbie-x360.corp.kruitzit.nl>
2019-10-27 00:04:54 +0200

    Refactor: Changed webcam.init to internal init (and initialize during construction

--- actors/webcam.js
+++ actors/webcam.js
@@ -1,4 +1,4 @@
-function WebCam() {
+function WebCam(video, canvas) {
     // The width and height of the captured photo. We will set the
     // width to the value defined here, but the height will be
     // calculated based on the aspect ratio of the input stream.
@@ -6,19 +6,10 @@ function WebCam() {
     var width = 640;    // We will scale the photo width to this
     var height = 0;     // This will be computed based on the input stream
   
-    // |streaming| indicates whether or not we're currently streaming
-    // video from the camera. Obviously, we start at false.
-  
     var streaming = false;
-  
-    // The various HTML elements we need to configure or control. These
-    // will be set by the startup() function.
-  
-    var video = null;
-    var canvas = null;
+    init();
 
     return {
-        init: init,
         canTakePicture: function() {
           return stream;
         },
@@ -26,10 +17,8 @@ function WebCam() {
         exec: getPicture
     };
   
-    function init(videoParam, canvasParam) {
-        console.log('init webcam');
-      video = videoParam;
-      canvas = canvasParam;
+    function init() {
+      console.log('init webcam');
       navigator.mediaDevices.getUserMedia({video: true, audio: false})
       .then(function(stream) {
         video.srcObject = stream;
