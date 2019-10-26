27cfa14b9b0b29a0a80a7284d7d4620b73f524f5
unknown <Debbie@nb-debbie-x360.corp.kruitzit.nl>
2019-10-27 00:54:37 +0200

    Added download btn to Finish step

--- actors/finish.js
+++ actors/finish.js
@@ -1,9 +1,32 @@
 function Finish(canvasStack) {
+    var canvas;
+    var downloadBtn = createDownloadBtn();
+
     var transformer = new TransformImage(
-        canvasStack.push('Finish'), 
+        canvas = canvasStack.push('Finish', {
+            title: 'Download',
+            action: function() {
+                var imgUrl = canvas.toDataURL('image/png');
+                download(imgUrl);
+            }
+        }), 
         data => { });
         
     return {
         exec: transformer.exec
     };
+
+    function download(dataUrl) {
+        downloadBtn.setAttribute('href', dataUrl);
+        downloadBtn.click();
+    }
+
+    function createDownloadBtn() {
+        var a = document.createElement('a');
+        a.setAttribute('download', 'img.png');
+        var body = document.getElementsByTagName('body')[0];
+
+        body.appendChild(a);
+        return a;
+    }
 }
